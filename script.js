2ec64567e8e855bdbca5b3a52c74ab9f1149129b
ThomassMathisen <thomasmathisen@hotmail.com>
2022-09-06 18:30:07 +0200

    Finished v1

---
+++ script.js
@@ -0,0 +1,137 @@
+const books = document.querySelector(".books");
+const addBook = document.querySelector(".add-book");
+const modal = document.querySelector("#modal");
+const span = document.querySelector(".close");
+
+addBook.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+span.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+const addBookForm = document.querySelector(".add-book-form")
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const data = new FormData(e.target)
+  let newBook = {}
+  for(let[name,value] of data) {
+    if(name === "book-read") {
+      newBook["book-read"] = true
+    }
+    else {
+      newBook[name] = value || "";
+    }
+  }
+
+  if(!newBook["book-read"]) {
+    newBook["book-read"] = false;
+  }
+  addBookToLibrary(
+    newBook["book-title"],
+    newBook["book-author"],
+    newBook["book-pages"],
+    newBook["book-read"]
+  );
+
+  addBookForm.reset();
+  modal.style.display = "none";
+});
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+   this.title = title;
+   this.author = author;
+   this.pages = pages;
+   this.read = read;
+   this.id = Math.floor(Math.random() * 100000000)
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+  saveAndRenderBooks();
+}
+
+function createBookItem(book, index) {
+  const bookItem = document.createElement("div");
+  bookItem.setAttribute("id", index);
+  bookItem.setAttribute("key", index);
+  bookItem.setAttribute("class", "card book");
+  bookItem.appendChild(
+    createBookElement("h1", `Title: ${book.title}`, "book-title")
+  );
+  bookItem.appendChild(
+    createBookElement("h1", `Author: ${book.author}`, "book-author")
+  );
+  bookItem.appendChild(
+    createBookElement("h1", `Pages: ${book.pages}`, "book-pages")
+  );
+  bookItem.appendChild(createReadElement(bookItem, book));
+  bookItem.appendChild(createBookElement("button", "X", "delete"));
+
+  bookItem.querySelector(".delete").addEventListener("click", () => {
+    deleteBook(index);
+  });
+
+  books.insertAdjacentElement("afterbegin", bookItem);
+}
+
+function renderBooks() {
+  books.textContent = "";
+  myLibrary.map((book, index) => {
+    createBookItem(book, index);
+  });
+}
+
+function saveAndRenderBooks() {
+  localStorage.setItem("library", JSON.stringify(myLibrary));
+  renderBooks();
+}
+
+function createBookElement(el, content, className) {
+  const element = document.createElement(el);
+  element.textContent = content;
+  element.setAttribute("class", className);
+  return element;
+}
+
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  saveAndRenderBooks();
+}
+
+function createBookElement(el, content, className) {
+  const element = document.createElement(el);
+  element.textContent = content;
+  element.setAttribute("class", className);
+  return element;
+}
+
+function createReadElement(bookItem, book) {
+  let read = document.createElement("div");
+  read.setAttribute("class", "book-read");
+  read.appendChild(createBookElement("h1", "Read?", "book-read-title"));
+  let input = document.createElement("input");
+  input.type = "checkbox";
+  input.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      bookItem.setAttribute("class", "card book read-checked");
+      book.read = true;
+      saveAndRenderBooks();
+    } else {
+      bookItem.setAttribute("class", "card book read-unchecked");
+      book.read = false;
+      saveAndRenderBooks();
+    }
+  });
+  if (book.read) {
+    input.checked = true;
+    bookItem.setAttribute("class", "card book read-checked");
+  }
+  read.appendChild(input);
+  return read;
+}
+
